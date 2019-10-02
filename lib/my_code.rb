25221eb7b65ebca732c256d526a01d8357c08e54
ZUBEYDE SISMAN <zubeydesisman@gmail.com>
2019-10-02 18:53:57 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+def map(array)
+  new =[]
+  i = 0
+  while  i< array.size 
+  new.push(yield(array[i])) 
+  i += 1 
+end
+  new
+end
+
+def reduce(array, sp = nil)
+  if sp  
+    sum = sp
+    i = 0 
+  else
+    sum = array[0]
+    i = 1 
+  end 
+  while i < array.size 
+  sum  = yield(sum,array[i])
+  i += 1  
+end
+  sum
+end
